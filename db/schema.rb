9a4f6f65368e76310a3c9ebd3b7fd03c13551c03
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-10-18 17:01:28 +0100

    Add author to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_192438) do
+ActiveRecord::Schema.define(version: 2019_09_27_162301) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_192438) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
