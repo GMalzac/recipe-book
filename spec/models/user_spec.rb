0a7c5819aa30f92d0f9f0ad17a1d2e74f4ee5cdd
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-12-01 18:39:43 +0000

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+
+  before(:all) do
+    @user1 ||= create(:user)
+    p "#{User.all}"
+    p @user1
+    p "oooo"
+  end
+
+
+  it "is valid with valid attributes" do
+    expect(@user1).to be_valid
+  end
+
+  it "has a unique username" do
+    user2 = build(:user, email: "bob@gmail.com")
+    expect(user2).to_not be_valid
+  end
+
+  it "has a unique email" do
+    user2 = build(:user, username: "Bob")
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user2 = build(:user, password: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid with a short password" do
+    user2 = build(:user, password: "blah")
+  end
+
+  it "is not valid without a username" do
+    user2 = build(:user, username: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without an email" do
+    user2 = build(:user, email: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a password matching the password_confirmation" do
+    user2 = build(:user, password: "blablabla", password_confirmation: "blablabl")
+    expect(user2).to_not be_valid
+  end
 end
