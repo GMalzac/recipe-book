ffa0b9cc7c02a816c4cc92ec09d8eb9a923a827e
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-12-01 19:09:35 +0000

    Add category tests and update user tests

---
+++ spec/factories/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :category do
+    name { "Desserts" }
+  end
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @category1 ||= create(:category)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@category1).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    category2 = build(:category, name: nil)
+    expect(category2). to_not be_valid
+  end
+
+  it "has a unique name" do
+    category2 = build(:category, name: "Desserts")
+    expect(category2).to_not be_valid
+  end
 end
