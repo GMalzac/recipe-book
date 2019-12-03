61997e20770088c271f65f10c890e22a1c7a25f8
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-12-03 19:42:28 +0000

    Add test for category associations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,17 +6,26 @@ RSpec.describe Category, type: :model do
     @category1 = create(:category)
   end
 
-  it "is valid with valid attributes" do
-    expect(@category1).to be_valid
+  describe "Associations" do
+    it 'has_many recipes' do
+      assc = described_class.reflect_on_association(:recipes)
+      expect(assc.macro).to eq :has_many
+    end
   end
 
-  it 'is not valid without a name' do
-    category2 = build(:category, name: nil)
-    expect(category2). to_not be_valid
-  end
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      expect(@category1).to be_valid
+    end
 
-  it "has a unique name" do
-    category2 = build(:category, name: "Desserts")
-    expect(category2).to_not be_valid
+    it 'is not valid without a name' do
+      category2 = build(:category, name: nil)
+      expect(category2). to_not be_valid
+    end
+
+    it "has a unique name" do
+      category2 = build(:category, name: "Desserts")
+      expect(category2).to_not be_valid
+    end
   end
 end
