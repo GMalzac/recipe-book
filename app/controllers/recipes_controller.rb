2f2641a8f455a000341ea80d90a4bc3c06e76c5e
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-12-10 02:12:01 +0000

    Add search bar to homepage

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,11 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :home, :show, :category, :get_recipe, :recipe_params]
 
   def index
-    @recipes = policy_scope(Recipe).order(created_at: :asc)
+    if params[:query].present?
+      @recipes = policy_scope(Recipe).search_by_title_and_ingredients_and_author(params[:query])
+    else
+      @recipes = policy_scope(Recipe).order(created_at: :asc)
+    end
     authorize @recipes
   end
 
