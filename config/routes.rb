45a492b37bac13ffc564296e2f53499942d0826b
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-12-15 14:26:13 +0000

    Update layout for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to:'recipes#home'
   devise_for :users
   resources :recipes do
     collection do
@@ -16,7 +17,6 @@ Rails.application.routes.draw do
 
   end
 
-  root 'recipes#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
